d860f29c87d1d3ec6cf91d8d02b0ea2769837afa
uz2ni <uzini_@naver.com>
2024-03-24 23:32:52 +0900

    chore: init setting

---
+++ src/main/java/com/example/yujapay/YujaPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.yujapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YujaPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YujaPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yujapay/YujaPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yujapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YujaPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
