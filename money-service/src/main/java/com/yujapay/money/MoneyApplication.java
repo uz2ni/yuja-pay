f687160ab3da59133e728479edaaaaa836f4a385
uz2ni <uzini_@naver.com>
2024-05-07 13:43:54 +0900

    chore: 머니 서비스 모듈 추가

---
+++ money-service/src/main/java/com/yujapay/money/MoneyApplication.java
@@ -0,0 +1,13 @@
+package com.yujapay.moneyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyServiceApplication.class, args);
+    }
+
+}
---
+++ money-service/src/test/java/com/yujapay/money/MoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujapay.moneyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
