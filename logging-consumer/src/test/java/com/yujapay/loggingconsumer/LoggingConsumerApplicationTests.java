f3d09826f2cd02f26a4f1723b347c75fdac22ac6
uz2ni <uzini_@naver.com>
2024-05-16 20:26:15 +0900

    feat: logging-consumer, task-consumer 서브모듈 구조 생성

---
+++ logging-consumer/src/test/java/com/yujapay/loggingconsumer/LoggingConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujapay.loggingconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ task-consumer/src/test/java/com/yujapay/taskconsumer/TaskConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujapay.taskconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
