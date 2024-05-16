7a02017e13eb360d449155139494c60da37b257a
uz2ni <uzini_@naver.com>
2024-05-16 20:35:01 +0900

    feat: 임포트 추가, task-consumer 관련 소스 추가

--- task-consumer/src/main/java/com/yujapay/taskconsumer/TaskConsumer.java
+++ task-consumer/src/main/java/com/yujapay/taskconsumer/TaskConsumer.java
@@ -1,5 +1,9 @@
-package com.yujapay.loggingconsumer;
+package com.yujapay.taskconsumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yujapay.common.RechargingMoneyTask;
+import com.yujapay.common.SubTask;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -8,15 +12,16 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 @Component
-public class LoggingConsumer {
-
+public class TaskConsumer {
     private final KafkaConsumer<String, String> consumer;
-    public LoggingConsumer(@Value("${kafka.clusters.bootstrapservers}") String bootstrapServers,
-                           @Value("${logging.topic}")String topic) {
-
+    private final TaskResultProducer taskResultProducer;
+    public TaskConsumer(@Value("${kafka.clusters.bootstrapservers}") String bootstrapServers,
+                        @Value("${task.topic}")String topic, TaskResultProducer taskResultProducer) {
+        this.taskResultProducer = taskResultProducer;
         Properties props = new Properties();
         props.put("bootstrap.servers", bootstrapServers);
         props.put("group.id", "my-group");
@@ -24,13 +29,29 @@ public class LoggingConsumer {
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         this.consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Collections.singletonList(topic));
-
         Thread consumerThread = new Thread(() -> {
             try {
                 while (true) {
+                    ObjectMapper mapper = new ObjectMapper();
                     ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
                     for (ConsumerRecord<String, String> record : records) {
                         System.out.println("Received message: " + record.value());
+
+                        try {
+                            RechargingMoneyTask tasks = mapper.readValue(record.value(), RechargingMoneyTask.class);
+                            List<SubTask> subTaskList = tasks.getSubTaskList();
+
+                            // validation membership
+                            // validation banking
+                            for (SubTask subTask : subTaskList) {
+                                subTask.setStatus("success");
+                            }
+
+                            this.taskResultProducer.sendTaskResult(tasks.getTaskID(), tasks);
+
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
                 }
             } finally {
