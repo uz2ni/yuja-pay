3bfc5e21f93121ecfd86795a59eb0761c2e4b045
uz2ni <uzini_@naver.com>
2024-07-05 14:19:16 +0900

    chore: 송금 모듈 소스 추가(누락분)

--- remittance-service/src/main/java/com/yujapay/remittance/adapter/in/web/FindRemittanceHistoryController.java
+++ remittance-service/src/main/java/com/yujapay/remittance/adapter/in/web/FindRemittanceHistoryController.java
@@ -1,4 +1,28 @@
 package com.yujapay.remittance.adapter.in.web;
 
+import com.yujapay.common.WebAdapter;
+import com.yujapay.remittance.application.port.in.FindRemittanceCommand;
+import com.yujapay.remittance.application.port.in.FindRemittanceUseCase;
+import com.yujapay.remittance.domain.RemittanceRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
 public class FindRemittanceHistoryController {
+
+    private final FindRemittanceUseCase findRemittanceUseCase;
+    @GetMapping( "/remittance/{membershipId}")
+    List<RemittanceRequest> findRemittanceHistory(@PathVariable String membershipId) {
+        FindRemittanceCommand command = FindRemittanceCommand.builder()
+                .membershipId(membershipId)
+                .build();
+
+        return findRemittanceUseCase.findRemittanceHistory(command);
+    }
 }
