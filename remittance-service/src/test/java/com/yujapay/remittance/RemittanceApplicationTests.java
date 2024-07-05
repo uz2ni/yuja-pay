143950f9dc878561652c09a4612729f2da69a1cb
uz2ni <uzini_@naver.com>
2024-07-05 12:56:19 +0900

    chore: 송금 서비스 서브모듈 추가

---
+++ remittance-service/src/test/java/com/yujapay/remittance/RemittanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujapay.remittance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemittanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
